b74049319120fb1f911a1f269112ea9cf420e818
KIIICH <KIIICH>
2021-09-05 22:20:23 +0300

    Settings logic

--- RGB/SettingsViewController.swift
+++ RGB/SettingsViewController.swift
@@ -9,21 +9,49 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    @IBOutlet weak var currentColor: UIView!
+    
+    @IBOutlet weak var redValueLabel: UILabel!
+    @IBOutlet weak var greenValueLabel: UILabel!
+    @IBOutlet weak var blueValueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setCurrentColor()
+        setColorValueLabels()
+        setupUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        setCurrentColor()
+        setColorValueLabels()
     }
-    */
+    
+    private func setCurrentColor() {
+        currentColor.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    private func setColorValueLabels() {
+        redValueLabel.text = formattedString(redSlider.value)
+        greenValueLabel.text = formattedString(greenSlider.value)
+        blueValueLabel.text = formattedString(blueSlider.value)
+    }
+    
+    private func setupUI() {
+        currentColor.layer.cornerRadius = 15
+    }
+    
+    private func formattedString(_ value: Float) -> String {
+        String(format: "%.2f", value)
+    }
 
 }
